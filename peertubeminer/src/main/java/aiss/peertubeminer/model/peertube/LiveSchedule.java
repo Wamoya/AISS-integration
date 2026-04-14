253e8c70c80af2e0ba27c792c8033abefc14c10a
mmunoza-edu <marmunavi@alum.us.es>
2026-04-14 08:18:26 +0200

    Add POJO classes to PeerTube model

---
+++ peertubeminer/src/main/java/aiss/peertubeminer/model/peertube/LiveSchedule.java
@@ -0,0 +1,45 @@
+
+package aiss.peertubeminer.model.peertube;
+
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "startAt"
+})
+@Generated("jsonschema2pojo")
+public class LiveSchedule {
+
+    @JsonProperty("startAt")
+    private String startAt;
+
+    @JsonProperty("startAt")
+    public String getStartAt() {
+        return startAt;
+    }
+
+    @JsonProperty("startAt")
+    public void setStartAt(String startAt) {
+        this.startAt = startAt;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(LiveSchedule.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("startAt");
+        sb.append('=');
+        sb.append(((this.startAt == null)?"<null>":this.startAt));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
