a6267494c637060aeb839cab62cbbe145587e942
Wamoya <juangrossodll@gmail.com>
2026-04-18 16:21:39 +0200

    Create repositories with test data Sample repositories were added for storing sample Video, Channel and Comment objects. Repositories for User and Caption objects are still to be added.

---
+++ peertubeminer/src/main/java/aiss/peertubeminer/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package aiss.peertubeminer.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    // TODO
+}
---
+++ peertubeminer/src/main/java/aiss/peertubeminer/repository/VideoRepository.java
@@ -0,0 +1,66 @@
+package aiss.peertubeminer.repository;
+
+import aiss.peertubeminer.model.peertube.Caption;
+import aiss.peertubeminer.model.peertube.Comment;
+import aiss.peertubeminer.model.peertube.Video;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class VideoRepository {
+    List<Video> videos = new ArrayList<>();
+    ChannelRepository channelRepository = new ChannelRepository();
+    CommentRepository commentRepository = new CommentRepository();
+
+
+    public VideoRepository() {
+        this.videos.add(
+                new Video(
+                        1,
+                        "title",
+                        "description",
+                        "yesterday",
+                        channelRepository.findOneById(1),
+                        new ArrayList<Comment>(
+                                List.of(commentRepository.findOneById(1),
+                                        commentRepository.findOneById(2))
+                                ),
+                        new ArrayList<Caption>()
+                )
+
+        );
+        this.videos.add(
+                new Video(
+                        2,
+                        "Different video",
+                        "Different video's description",
+                        "tomorrow",
+                        channelRepository.findOneById(1),
+                        new ArrayList<Comment>(),
+                        new ArrayList<Caption>()
+                        )
+        );
+        this.videos.add(
+                new Video(
+                        3,
+                        "Java 101,",
+                        "Haskell is better.",
+                        "sad",
+                        channelRepository.findOneById(3),
+                        new ArrayList<Comment>(List.of(commentRepository.findOneById(3))),
+                        new ArrayList<Caption>()
+                )
+        );
+    }
+
+    public List<Video> findAll() { return this.videos; }
+
+    public Video findOneById(Integer id) {
+        return this.videos.stream()
+                .filter(v -> v.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
