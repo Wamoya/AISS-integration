9e7013ec4755e793479edda1cfb365dd7d1f8a99
mmunoza-edu <marmunavi@alum.us.es>
2026-05-05 10:29:32 +0200

    Completed OAS annotations for PTM and DMM controllers.
    -Added necessary dependencies for PTM and DMM

--- dailymotionminer/src/main/java/aiss/dailymotionminer/controller/DailymotionController.java
+++ dailymotionminer/src/main/java/aiss/dailymotionminer/controller/DailymotionController.java
@@ -5,11 +5,20 @@ import aiss.dailymotionminer.model.dailymotion.Channel;
 import aiss.dailymotionminer.model.videominer.VM_Channel;
 import aiss.dailymotionminer.service.ChannelService;
 import aiss.dailymotionminer.service.VideoMinerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "DailyMotion Channel", description = "Channel management in DailyMotion")
 @RestController
 @RequestMapping("/api/dailymotionminer/v1")
 public class DailymotionController {
@@ -19,16 +28,37 @@ public class DailymotionController {
     @Autowired
     VideoMinerService videoMinerService;
 
+    @Operation(
+            summary = "Retrieve a channel",
+            description = "Get a channel from DailyMotion",
+            tags = {"GET"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = VM_Channel.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema())})
+    })
     @GetMapping("/{channelName}")
-    public VM_Channel getChannel(@PathVariable String channelName,
+    public VM_Channel getChannel(@Parameter(description = "Name of the DailyMotion channel to be searched") @PathVariable String channelName,
                                  @RequestParam(name = "maxVideos", defaultValue = "10") @Min(0) @Max(1000) Integer maxVideos, //The Dailymotion API allows for a maximum of 10 pages with a maximum 100 videos per page, so our API will be able to request at most 1000 videos from a channel.
                                  @RequestParam(name = "maxComments", defaultValue = "2") @Min(0) Integer maxComments) {
         Channel channel = channelService.getChannelWithVideos(channelName, maxVideos, maxComments);
         return Transformer.toVMChannel(channel);
     }
 
+    @Operation(
+            summary = "Insert a channel into VideoMiner",
+            description = "Insert a DailyMotion Channel object into VideoMiner",
+            tags = {"POST"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = { @Content(schema = @Schema(implementation = VM_Channel.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema())})
+    })
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{channelName}")
-    public VM_Channel postChannel(@PathVariable String channelName,
+    public VM_Channel postChannel(@Parameter(description = "Name of the DailyMotion channel to be inserted into VideoMiner") @PathVariable String channelName,
                                   @RequestParam(name = "maxVideos", defaultValue = "10") @Min(0) @Max(1000) Integer maxVideos,
                                   @RequestParam(name = "maxComments", defaultValue = "2") @Min(0) Integer maxComments) {
         Channel channel = channelService.getChannelWithVideos(channelName, maxVideos, maxComments);
--- peertubeminer/src/main/java/aiss/peertubeminer/controller/PeertubeController.java
+++ peertubeminer/src/main/java/aiss/peertubeminer/controller/PeertubeController.java
@@ -5,10 +5,19 @@ import aiss.peertubeminer.model.peertube.*;
 import aiss.peertubeminer.model.videominer.*;
 import aiss.peertubeminer.service.ChannelService;
 import aiss.peertubeminer.service.VideoMinerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "PeerTube Channel", description = "Channel management in PeerTube")
 @RestController
 @RequestMapping("/api/peertubeminer/v1")
 public class PeertubeController {
@@ -18,16 +27,36 @@ public class PeertubeController {
     @Autowired
     VideoMinerService videoMinerService;
 
+    @Operation(
+            summary = "Retrieve a channel",
+            description = "Get a channel from PeerTube",
+            tags = {"GET"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = VM_Channel.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema())})
+    })
     @GetMapping("/{channelName}")
-    public VM_Channel getChannel(@PathVariable String channelName,
+    public VM_Channel getChannel(@Parameter(description = "Name of the PeerTube channel to be searched") @PathVariable String channelName,
                                  @RequestParam(name = "maxVideos", defaultValue = "10") @Min(0) Integer maxVideos,
                                  @RequestParam(name = "maxComments", defaultValue = "2") @Min(0) Integer maxComments) {
         Channel channel = channelService.getChannelWithVideos(channelName, maxVideos, maxComments);
         return Transformer.toVMChannel(channel);
     }
 
+    @Operation(
+            summary = "Insert a channel into VideoMiner",
+            description = "Insert a PeerTube Channel object into VideoMiner",
+            tags = {"POST"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = { @Content(schema = @Schema(implementation = VM_Channel.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema())})
+    })
     @PostMapping("/{channelName}")
-    public VM_Channel postChannel(@PathVariable String channelName,
+    public VM_Channel postChannel(@Parameter(description = "Name of the PeerTube channel to be inserted into VideoMiner") @PathVariable String channelName,
                                   @RequestParam(name = "maxVideos", defaultValue = "10") @Min(0) Integer maxVideos,
                                   @RequestParam(name = "maxComments", defaultValue = "2") @Min(0) Integer maxComments) {
         Channel channel = channelService.getChannelWithVideos(channelName, maxVideos, maxComments);
